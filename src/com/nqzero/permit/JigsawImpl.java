fc5147e2add6cb65ca2ffa3ca11ce92297342561
lytles@takashi <srl.source@nqzero.com>
2019-03-28 11:32:08 -0400

    godmode - allow specifying a starting class

--- src/com/nqzero/permit/JigsawImpl.java
+++ src/com/nqzero/permit/JigsawImpl.java
@@ -9,15 +9,18 @@ public class JigsawImpl {
     static boolean dbg = false;
 
     public static void godMode() {
+        godMode(Safer.class);
+    }
+    public static void godMode(Class first) {
         try {
             Method export = Module.class.getDeclaredMethod("implAddOpens",String.class);
             Permit.setAccessible(export);
             HashSet<Module> modules = new HashSet();
-            Module base = Safer.class.getModule();
+            Module base = first.getModule();
             if (base.getLayer() != null)
                 modules.addAll(base.getLayer().modules());
             modules.addAll(ModuleLayer.boot().modules());
-            for (ClassLoader cl = Safer.class.getClassLoader(); cl != null; cl = cl.getParent()) {
+            for (ClassLoader cl = first.getClassLoader(); cl != null; cl = cl.getParent()) {
                 modules.add(cl.getUnnamedModule());
             }
             for (Module module : modules) {
